f71275ea00bf76b0318abcb44afaaa1716a7405e
Clonkk <rf.clonk@linuxmail.org>
2021-07-07 17:39:59 +0200

    swap to Tensor type for poly2d

--- polynumeric/poly2d.nim
+++ polynumeric/poly2d.nim
@@ -1,61 +1,24 @@
-import math
+import arraymancer
+import std/math
 
 type
   TwoVarFunction* = proc (x, y: float): float
 
   Poly2d* = object
-    coefs: seq[tuple[x, y: float]]
+    coefs: Tensor[float]
 
-proc degree*(p: Poly2d): int =
-  ## Returns the degree of the polynomial,
-  ## that is the number of coefficients-1
-  return p.coefs.len - 1
-
-proc eval*(p: Poly2d, args: tuple[x, y: float]): float =
-  ## Evaluates a polynomial function value for `x`
-  ## quickly using Horners method
-  var n = p.degree
-  result = 0.0
-  for i in countdown[int](n, 0):
-    for j in countdown[int](n, 0):
-      echo (i, j)
-      let tmp = pow(args.x, i.toFloat) * p.coefs[i].x * pow(args.y, j.toFloat) * p.coefs[j].y
-      result += tmp
+proc initPoly2d*(coefs: Tensor[float]) : Poly2d =
+  result.coefs = coefs
 
 proc eval*(p: Poly2d, x, y: float): float =
-  p.eval((x: x, y: y))
+  ## Evaluates a polynomial function value for `x`
+  var
+    nx = p.coefs.shape[0] - 1
+    ny = p.coefs.shape[1] - 1
+  result = 0.0
+  for i in countdown[int](nx, 0):
+    for j in countdown[int](ny, 0):
+      result += pow(x, i.toFloat) * pow(y, j.toFloat) * (p.coefs[i, j])
 
-proc `[]` *(p: Poly2d, idx: int): tuple[x, y: float] =
-  ## Gets a coefficient of the polynomial.
-  ## p[2] will returns the quadric term, p[3] the cubic etc.
-  ## Out of bounds index will return 0.0.
-  if idx < 0 or idx > p.degree:
-    return (x: 0.0, y: 0.0)
-
-  result.x = p.coefs[idx].x
-  result.y = p.coefs[idx].y
-
-proc `[]=` *(p: var Poly2d; idx: int, v: tuple[x, y: float]) =
-  ## Sets an coefficient of the polynomial by index.
-  ## p[2] set the quadric term, p[3] the cubic etc.
-  ## If index is out of range for the coefficients,
-  ## the polynomial grows to the smallest needed degree.
-  assert(idx >= 0)
-
-  if idx > p.degree: #polynomial must grow
-    var oldlen = p.coefs.len
-    p.coefs.setLen(idx + 1)
-    for q in oldlen..<high(p.coefs):
-      p.coefs[q].x = 0.0 # new-grown coefficients set to zero
-      p.coefs[q].y = 0.0 # new-grown coefficients set to zero
-
-  p.coefs[idx] = v
-
-iterator items*(p: Poly2d): tuple[x, y: float] =
-  ## Iterates through the coefficients of the polynomial.
-  var i = p.degree
-  while i >= 0:
-    yield p[i]
-    dec i
-
-
+proc eval*(p: Poly2d, args: tuple[x, y: float]): float =
+  p.eval(args.x, args.y)
--- tests/t_poly2d.nim
+++ tests/t_poly2d.nim
@@ -1,25 +1,16 @@
 import unittest
+
+import arraymancer
 import ../polynumeric/poly2d
 
-
 proc test() =
-  var p_coefs = @[
-    (x: -2.0, y: 3.0),
-    (x: 0.5, y: 1.2),
-    (x: 1.2, y: -1.3),
-  ]
+  var coefs = [[-2.0, 3.0],
+          [0.5, 1.2],
+          [1.2, -1.3],
+          [0.77, 0.88],
+          ].toTensor
 
-  var p : Poly2d
-  p[0] = (x: -2.0, y: 3.0)
-  p[1] = (x: 0.5, y: 1.2)
-  p[2] = (x: 1.2, y: -1.3)
-  check p[0] == (x: -2.0, y: 3.0)
-  check p[1] == (x: 0.5, y: 1.2)
-  check p[2] == (x: 1.2, y: -1.3)
-
-  p[3] = (x: 0.77, y: 0.88)
-  check p[3] == (x: 0.77, y: 0.88)
-
+  var p = initPoly2d(coefs)
   block:
     var res = p.eval((x: 3.07, y: 4.05))
     echo res
